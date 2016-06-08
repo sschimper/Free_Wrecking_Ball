8492e574796eb4bc240d5b8dcd5f2dd4205516a4
Thorge Neelsen <thorgeneelsen@gmail.com>
2016-06-08 20:28:03 +0200

    Update von Thorge

--- app/src/main/java/com/example/sebastian/gatav_project/MainActivity.java
+++ app/src/main/java/com/example/sebastian/gatav_project/MainActivity.java
@@ -1,89 +1,235 @@
 package com.example.sebastian.gatav_project;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.net.Uri;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.Display;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
-import android.view.Surface;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Created by Sebastian on 01.06.2016.
  */
 public class MainActivity extends MainMenue {
 
-
-    private GoogleApiClient client;
+    private MyGLSurfaceView glSurfaceView;
+    public static CubeRenderer cube;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        MyGLSurfaceView view = new MyGLSurfaceView(this);
-        view.context=this;
-        setContentView(view);
+        setContentView(R.layout.main_activity);
 
-        //Toast.makeText(context, "Level 1", Toast.LENGTH_SHORT).show();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
+        cube = new CubeRenderer(true);
 
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.sebastian.gatav_project/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+        glSurfaceView = (MyGLSurfaceView) findViewById(R.id.GLView);
+        glSurfaceView.setZOrderOnTop(true);
+        glSurfaceView.setEGLConfigChooser(8,8,8,8,16,0);
+        glSurfaceView.getHolder().setFormat(PixelFormat.RGBA_8888);
+        glSurfaceView.setRenderer(cube);
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.sebastian.gatav_project/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
+    protected void onResume() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onResume();
     }
 
+    @Override
+    protected void onPause() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onPause();
+    }
+
+}
+
+/**
+ * A vertex shaded cube.
+ */
+class Cube {
+    public Cube() {
+        int one = 0x10000;
+        int vertices[] = { -one, -one, -one, one, -one, -one, one, one, -one,
+                -one, one, -one, -one, -one, one, one, -one, one, one, one,
+                one, -one, one, one, };
+
+        int colors[] = { 0, 0, 0, one, one, 0, 0, one, one, one, 0, one, 0,
+                one, 0, one, 0, 0, one, one, one, 0, one, one, one, one, one,
+                one, 0, one, one, one, };
+
+        byte indices[] = { 0, 4, 5, 0, 5, 1, 1, 5, 6, 1, 6, 2, 2, 6, 7, 2, 7,
+                3, 3, 7, 4, 3, 4, 0, 4, 7, 6, 4, 6, 5, 3, 0, 1, 3, 1, 2 };
+
+        // Buffers to be passed to gl*Pointer() functions
+        // must be direct, i.e., they must be placed on the
+        // native heap where the garbage collector cannot
+        // move them.
+        //
+        // Buffers with multi-byte datatypes (e.g., short, int, float)
+        // must have their byte order set to native order
+
+        ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+        vbb.order(ByteOrder.nativeOrder());
+        mVertexBuffer = vbb.asIntBuffer();
+        mVertexBuffer.put(vertices);
+        mVertexBuffer.position(0);
+
+        ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+        cbb.order(ByteOrder.nativeOrder());
+        mColorBuffer = cbb.asIntBuffer();
+        mColorBuffer.put(colors);
+        mColorBuffer.position(0);
+
+        mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
+        mIndexBuffer.put(indices);
+        mIndexBuffer.position(0);
+    }
+
+    public void draw(GL10 gl) {
+        gl.glFrontFace(GL10.GL_CW);
+        gl.glVertexPointer(3, GL10.GL_FIXED, 0, mVertexBuffer);
+        gl.glColorPointer(4, GL10.GL_FIXED, 0, mColorBuffer);
+        gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_BYTE,
+                mIndexBuffer);
+    }
+
+    private IntBuffer mVertexBuffer;
+    private IntBuffer mColorBuffer;
+    private ByteBuffer mIndexBuffer;
+}
+
+/**
+ * Render a pair of tumbling cubes.
+ */
+
+class CubeRenderer implements GLSurfaceView.Renderer {
+
+    public float x;
+    public float y;
+
+    public CubeRenderer(boolean useTranslucentBackground) {
+        mTranslucentBackground = useTranslucentBackground;
+        mCube = new Cube();
+    }
+
+    public void onDrawFrame(GL10 gl) {
+    /*
+     * Usually, the first thing one might want to do is to clear the screen.
+     * The most efficient way of doing this is to use glClear().
+     */
+
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+    /*
+     * Now we're ready to draw some 3D objects
+     */
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0, 0, -3.0f);
+        gl.glRotatef(y, 1, 0, 0);
+        gl.glRotatef(x, 0, 1, 0);
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
+        mCube.draw(gl);
+
+        mAngle += 1.2f;
+    }
+
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width, height);
+
+    /*
+     * Set our projection matrix. This doesn't have to be done each time we
+     * draw, but usually a new projection needs to be set when the viewport
+     * is resized.
+     */
+
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+    }
+
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+    /*
+     * By default, OpenGL enables features that improve quality but reduce
+     * performance. One might want to tweak that especially on software
+     * renderer.
+     */
+        gl.glDisable(GL10.GL_DITHER);
+
+    /*
+     * Some one-time OpenGL initialization can be made here probably based
+     * on features of this particular context
+     */
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+
+        if (mTranslucentBackground) {
+            gl.glClearColor(0, 0, 0, 0);
+        } else {
+            gl.glClearColor(1, 1, 1, 1);
+        }
+        gl.glEnable(GL10.GL_CULL_FACE);
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+    }
+
+    private boolean mTranslucentBackground;
+    private Cube mCube;
+    private float mAngle;
+
+    public float getX() {
+        return x;
+    }
+}
+
+class MyGLSurfaceView extends GLSurfaceView{
+
+    private float mPreviousX;
+    private float mPreviousY;
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+
+
+
+    public MyGLSurfaceView(Context context) {
+        super(context);
+    }
+
+    public MyGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+
+        float x = e.getX();
+        float y = e.getY();
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+                MainActivity.cube.x += dx * TOUCH_SCALE_FACTOR;
+                MainActivity.cube.y += dy * TOUCH_SCALE_FACTOR;
+                requestRender();
+        }
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
+    }
 }
 
--- app/src/main/java/com/example/sebastian/gatav_project/MainMenue.java
+++ app/src/main/java/com/example/sebastian/gatav_project/MainMenue.java
@@ -1,10 +1,10 @@
 package com.example.sebastian.gatav_project;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,7 +13,7 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainMenue extends AppCompatActivity {
+public class MainMenue extends Activity {
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -43,45 +43,5 @@ public class MainMenue extends AppCompatActivity {
     public void quitGame(View v) {
         finish();
         System.exit(0);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "MainMenue Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.sebastian.gatav_project/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "MainMenue Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.sebastian.gatav_project/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
